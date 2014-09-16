55fcd9fac058763fe3afd91453b90c5aaac05c05
jeffdc <jeff@nothoo.com>
2014-09-15 23:29:03 -0400

    mostly finished up ch5. 5.13 and 5.16 are not complete.

--- src/main/scala/com/nothoo/Ch5/Ch5.scala
+++ src/main/scala/com/nothoo/Ch5/Ch5.scala
@@ -63,6 +63,42 @@ sealed trait Stream[+A] {
   def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(empty[B])((a,b) => f(a) append b)
 
   // 5.13
+  def map13[B](f: A => B): Stream[B] = unfold(this){
+    case Cons(h,t) => Some(f(h()), t())
+    case _ => None
+  }
+  def take13(n: Int): Stream[A] = unfold(this, n) {
+    case (Cons(h,t), n) if n == 1 => Some(h(), (empty,n-1))
+    case (Cons(h,t), n) if n > 0  => Some(h(), (t(),n-1))
+    case _ => None
+  }
+  def takeWhile13(p: A => Boolean): Stream[A] = unfold(this) {
+    case Cons(h,t) if p(h()) => Some(h(),t())
+    case _ => None
+  }
+  def zipWith[B,C](o: Stream[B])(f: (A,B) => C): Stream[C] = unfold(this,o) {
+    case (Cons(lh,lt), Cons(rh,rt)) => Some((f(lh(), rh()), (lt(), rt())))
+    case _ => None
+  }
+  // this one is a toughy, need to think about it more
+//  def zipAll[B](o: Stream[B]): Stream[(Option[A],Option[B])] = unfold(this,o) {
+//    case (Empty, Empty) => None
+//    case (Cons(h,t), Empty) => Some((h(),t()), )
+//  }
+
+  // 5.14 -- this is ugly, probably need zipAll
+  def startsWith[A](o: Stream[A]): Boolean = zipWith(o)(_ == _).takeWhile(b => b).toList.size > 0
+
+  // 5.15
+  def tails: Stream[Stream[A]] = unfold(this) {
+    case Empty => None
+    case s => Some(s, s drop 1)
+  } append Stream(empty)
+
+  def hasSubsequence[A](s: Stream[A]): Boolean = tails exists(_ startsWith s)
+
+  // 5.16 - not sure, does not seem that unfold is correct
+  def scanRight[B](x: => B)(f: (A, => B) => B): Stream[B] = ???
 }
 
 case object Empty extends Stream[Nothing]
@@ -94,10 +130,6 @@ object Stream {
 
   // 5.12
   def constant2[A](a: A): Stream[A] = unfold(a){ x => Some(x,x) }
-
-  // 5.13
-
-
 }
 
 object Ch5 extends App {
@@ -154,4 +186,24 @@ object Ch5 extends App {
 
   val ones: Stream[Int] = constant2(1)
   assert(List(1,1,1) == ones.take(3).toList)
+
+  // 5.13
+  assert(TEST_STREAM.map13(identity).toList == TEST_STREAM.map(identity).toList)
+  assert(TEST_STREAM.take13(2).toList == TEST_STREAM.take(2).toList)
+  assert(TEST_STREAM.takeWhile13(_>2).toList ==TEST_STREAM.takeWhile(_>2).toList)
+  assert(List("ab", "cd") == Stream("a", "c").zipWith(Stream("b", "d"))(_ + _).toList)
+
+  // 5.14
+  assert(TEST_STREAM startsWith Stream(1,2))
+  assert(!TEST_STREAM.startsWith(Stream(2,3)))
+  assert(TEST_STREAM startsWith Stream(1))
+  assert(!TEST_STREAM.startsWith(empty))
+
+  // 5.15
+  assert(List(List(1,2,3),List(2,3),List(3), List()) == Stream(1,2,3).tails.map(_.toList).toList)
+
+  // 5.16
+  //TODO
+//  assert(List(6,5,3,0) == Stream(1,2,3).scanRight(0)(_+_).toList)
+
 }
